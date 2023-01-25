8d8e871031e0046b1f06fb3bf210d4e9bd526fe3
NotInRealLife <gg.nft.game@gmail.com>
2023-01-25 15:15:25 +0700

    update

---
+++ 100.js
@@ -0,0 +1,23 @@
+var data=[
+    {
+        "id": 001,
+        "name": "ddd",
+        "cmine level": 0.0,
+        "type": "ddd",
+
+    },
+    {
+        "id": 002,
+        "name": 59.9,
+        "cmine level": 0.0,
+        "type": "ddd",
+
+    },
+    {
+        "id": 003,
+        "name": "ffff",
+        "cmine level": 0.0,
+        "type": "ddd",
+
+    },
+];
